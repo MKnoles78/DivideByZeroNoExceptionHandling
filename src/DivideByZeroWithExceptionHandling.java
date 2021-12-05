c56a65329a74025dadf2eae87c0c30b6b62ecf34
Mike Knoles <MKnoles78@gmail.com>
2021-12-04 20:52:25 -0500

    added new line character

--- src/DivideByZeroWithExceptionHandling.java
+++ src/DivideByZeroWithExceptionHandling.java
@@ -16,18 +16,18 @@ public class DivideByZeroWithExceptionHandling {
 		boolean continueLoop = true; // determines if more input is needed
 
 		do {
-			try {
-				// read two numbers and calculate quotient
-				System.out.print("Please enter numerator: ");
+			try // read two numbers and calculate quotient
+			{
+				System.out.print("Please enter an integer numerator: ");
 				int numerator = scanner.nextInt();
-				System.out.print("Please enter denominator: ");
+				System.out.print("Please enter an integer denominator: ");
 				int denominator = scanner.nextInt();
 
 				int result = quotient(numerator, denominator);
 				System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
 				continueLoop = false; // input successful; end looping
 			} catch (InputMismatchException inputMismatchException) {
-				System.err.printf("%Exception: %s%n", inputMismatchException);
+				System.err.printf("%nException: %s%n", inputMismatchException);
 				scanner.nextLine(); // discard input so user can try again
 				System.out.printf("You must enter integers. Please try again. %n%n");
 			} catch (ArithmeticException arithmeticException) {
@@ -36,4 +36,4 @@ public class DivideByZeroWithExceptionHandling {
 			}
 		} while (continueLoop);
 	}
-}
+} // end class DivideByZeroWithExceptionHandling
